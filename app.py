750791f2d62c38cb694c40770fc39874b9b4379a
Sanskarv417 <88963619+Sanskarv417@users.noreply.github.com>
2023-07-07 08:54:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, session, render_template, request, make_response, redirect, flash, jsonify
+from flask.helpers import url_for
+# from flask_mysql import MySQL
+# from flaskext.mysql import MySQL
+# from datetime import date
+# from flask_mail import *
+# from random import *
+# import pdfkit
+import os
+import pandas as pd
+import pickle
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    model_file = request.form['modelfile']
+    
+    #######work on model goes here########
+    return render_template('dashboard.html')
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/login', methods=['POST'])
+def login():
+    file = request.form['file']
+    dataset = pd.read_csv(file)
+
+    X_train = pd.read_csv("/Users/sanskar.verma/Downloads/Credit_risk_detection/train.csv")
+    scaler = StandardScaler().fit(X_train)
+
+    # Perform any necessary preprocessing on the dataset
+    data = scaler.transform(dataset)
+
+    # Make predictions using the loaded model
+    predictions = model.predict(data)
+    print('ok')
+    return render_template('dashboard.html', predictions=predictions)
+
+if __name__ == "__main__":
+    app.run(debug=True)
